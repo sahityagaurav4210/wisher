85ebc7ef5a0efc6e3b7162c9aa6500e580245f97
gauravsahitya32 <gaurav.sahitya@digimantra.com>
2023-04-26 00:21:48 +0530

    added scripts and remote db configs

--- db/config/config.js
+++ db/config/config.js
@@ -1,12 +1,18 @@
-require('dotenv/config');
+require("dotenv/config");
 const { DB_HOST, DB_PASS, DB_NAME, HOST, DIALECT } = process.env;
 
 module.exports = {
-  "development": {
-    "username": DB_HOST,
-    "password": DB_PASS,
-    "database": DB_NAME,
-    "host": HOST,
-    "dialect": DIALECT
-  }
-}
+  development: {
+    username: DB_HOST,
+    password: DB_PASS,
+    database: DB_NAME,
+    host: HOST,
+    dialect: DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
